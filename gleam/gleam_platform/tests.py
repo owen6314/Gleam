0a3c3a650ebfdefda6839b79838e5c76f1ab8a2f
owen <thss15_zhangqr@163.com>
2017-12-06 20:26:04 +0800

    添加主页测试

--- gleam/gleam_platform/tests.py
+++ gleam/gleam_platform/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+
+# 测试主页
+class IndexTest(TestCase):
+
+
+    # 测试主页url
+    def test_index_url(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual(response.status_code, 200)
+        response = c.get('/index')
+        self.assertEqual(response.status_code, 200) 
+
+
+'''
+class SignupOrganizerTest(TestCase):
+    
+
+    # 测试组织者注册失败跳转回主页
+    def test_signup_organizer_fail_url(self):
+        c = Client()
+        response = c.post('/signup/organizer')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/index')
+    
+    # 测试组织者注册成功跳转到组织者主页
+    def test_signup_organizer_success_url(self):
+        c = Client()
+        response = c.post('/signup/organizer',{"password":"12345678admin", "email": "thss@163.com"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/home/organizer')
+
+    def test_signup_organizer_info(self):
+        c = Client()
+        response = c.post('/signup/organizer',{"password":"12345678admin", "email": "thss@163.com"})
+'''       
+
+
+
+
+
