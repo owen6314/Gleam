040b6dd257bf1412c6e3ec224b0e6737a0068699
朱泓宇 <hy-zhu15@mails.tsinghua.edu.cn>
2017-12-07 14:35:58 +0800

    拆分url

---
+++ gleam/gleam_platform/contestant_account_views.py
@@ -0,0 +1,205 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from django.core.mail import EmailMessage
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+
+from .forms import UserSignupForm, UserLoginForm, ProfileContestantForm
+from .models import Tournament, Contestant, User, Image
+
+import gleam_platform.tools as tool
+from gleam import settings
+
+class SignupContestantView(View):
+  # 注册参赛者
+  # email password
+  @staticmethod
+  def post(request):
+    form = UserSignupForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+
+      # 未进行邮箱激活
+      user.is_active = False
+
+      # 设置用户类型
+      user.type = 'C'
+
+      # 连接用户类型对应的用户信息表单
+      profile = Contestant.objects.create()
+      user.contestant_profile = profile
+
+      user.save()
+
+      current_site = get_current_site(request)
+      mail_subject = '激活Gleam账户，迎接美丽新世界'
+      message = render_to_string('email_confirmation.html', {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': tool.account_activation_token.make_token(user),
+      })
+      to_email = form.cleaned_data.get('email')
+      email = EmailMessage(
+        mail_subject, message, settings.EMAIL_FROM, to=[to_email]
+      )
+      email.send()
+
+      # return render(request, 'email_activate.html', {'user_id': user.id, 'domain': 'http://'+ current_site})
+      return redirect('confirmation-email-send', user.id)
+
+    # 跳转到index
+    return redirect('index')
+
+
+class SendConfirmationEmailView(View):
+  @staticmethod
+  def get(request, user_id):
+    try:
+      user = User.objects.get(id=user_id)
+    except ObjectDoesNotExist:
+      return redirect('404')
+
+    current_site = get_current_site(request)
+    mail_subject = '激活Gleam账户，迎接美丽新世界'
+    message = render_to_string('email_confirmation.html', {
+      'user': user,
+      'domain': current_site.domain,
+      'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+      'token': tool.account_activation_token.make_token(user),
+    })
+    email = EmailMessage(
+      mail_subject, message, settings.EMAIL_FROM, to=[user.email]
+    )
+    email.send()
+
+    return render(request, 'email_activate.html', {'user_id': user.id, 'domain': 'http://' + current_site.domain})
+
+class LoginContestantView(View):
+  # 参赛方登录
+  # email password
+  @staticmethod
+  def post(request):
+    form = UserLoginForm(request.POST)
+    if form.is_valid():
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+      # 验证密码 和 用户类型
+      user = authenticate(username=email, password=password)
+      if user is not None and user.type == 'C':
+        login(request, user)
+        # 跳转到主页
+        return redirect('home-contestant')
+
+    # 跳转到index
+    return redirect('index')
+
+
+@method_decorator(login_required, name='dispatch')
+class LogoutContestantView(View):
+  # 登出
+  @staticmethod
+  def get(request):
+    auth.logout(request)
+    return redirect('index')
+
+
+@method_decorator(login_required, name='dispatch')
+class HomeContestantView(View):
+  # 显示参赛者主页
+  @staticmethod
+  def get(request):
+    try:
+      contestant = request.user.contestant_profile
+    except:
+      # 403 permission denied
+      return redirect('index')
+
+    # 当前所有发布的锦标赛，按注册时间倒序排列
+    # tournaments = Tournament.objects.filter(status__in=[Tournament.STATUS_PUBLISHED]).order_by('-register_begin_time')
+
+    data = dict()
+    data['tournaments'] = Tournament.objects.filter(team__members=request.user.contestant_profile).distinct()
+
+    return render(request, 'contestant_home.html', data)
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileContestantView(View):
+  # 显示参赛者信息
+  @staticmethod
+  def get(request, user_id):
+    # # 如果用户类型不符, 拒绝请求
+    # if request.user.type != 'C':
+    #   return redirect('permission-denied-403')
+    try:
+      user = User.objects.get(id=user_id)
+    except:
+      return redirect('404')
+
+    if user.type != 'C':
+      return redirect('403')
+
+    fields = ['nick_name', 'gender', 'school', 'introduction']
+    data = tool.load_model_obj_data_to_dict(user.contestant_profile, fields)
+    data['email'] = user.email
+    data['user'] = user
+
+    return render(request, 'contestant_profile.html', data)
+
+
+def activate(request, uidb64, token):
+  try:
+    uid = force_text(urlsafe_base64_decode(uidb64))
+    user = User.objects.get(pk=uid)
+  except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+    user = None
+  if user is not None and tool.account_activation_token.check_token(user, token):
+    user.is_active = True
+    user.save()
+    login(request, user)
+    return redirect('index')
+  else:
+    return HttpResponse('垃圾验证码!')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileEditContestantView(View):
+  @staticmethod
+  def get(request):
+    fields = ['nick_name', 'school', 'gender', 'introduction', 'resident_id']
+    data = tool.load_model_obj_data_to_dict(request.user.contestant_profile, fields)
+    form = ProfileContestantForm(initial=data)
+    return render(request, 'contestant_profile_edit.html', {'form': form})
+
+  @staticmethod
+  def post(request):
+    form = ProfileContestantForm(request.POST, request.FILES)
+    if form.is_valid():
+      # 保存除avatar之外的所有field
+      fields = ['nick_name', 'school', 'gender', 'introduction', 'resident_id']
+      tool.save_form_data_to_model_obj(request.user.contestant_profile, form, fields)
+
+      # 保存avatar
+      avatar = Image()
+      avatar.image = form.cleaned_data['avatar']
+      avatar.type = 'P'
+      avatar.owner = request.user
+      avatar.save()
+      request.user.contestant_profile.avatar = avatar
+      request.user.contestant_profile.save()
+
+      return redirect('profile-contestant', request.user.id)
+    else:
+      return render(request, 'contestant_profile_edit.html', {'form': form})
+
+
+
---
+++ gleam/gleam_platform/organizer_account_views.py
@@ -0,0 +1,182 @@
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views import View
+from django.utils.decorators import method_decorator
+
+from django.utils import timezone
+from .forms import UserSignupForm, UserLoginForm, ProfileOrganizerForm
+from .models import Tournament, Organizer, User, Image
+
+import gleam_platform.tools as tool
+
+
+class SignupOrganizerView(View):
+  # 注册赛事方
+  # email password
+  @staticmethod
+  def post(request):
+    form = UserSignupForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+
+      # 设置用户类型
+      user.type = 'O'
+
+      # 连接用户类型对应的用户信息表单
+      profile = Organizer.objects.create()
+      user.organizer_profile = profile
+      user.save()
+
+      # 注册完成后，直接登录
+      login(request, user)
+      return redirect('home-organizer')
+    return redirect('index')
+
+
+class LoginOrganizerView(View):
+  # 赛事方登录
+  # email password
+  @staticmethod
+  def post(request):
+    form = UserLoginForm(request.POST)
+    if form.is_valid():
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+      # 验证密码 和 用户类型
+      user = authenticate(username=email, password=password)
+      if user is not None and user.type == 'O':
+        login(request, user)
+        # 跳转到主页
+        return redirect('home-organizer')
+
+    # 跳转到index
+    return redirect('index')
+
+
+@method_decorator(login_required, name='dispatch')
+class LogoutOrganizerView(View):
+  # 登出
+  @staticmethod
+  def get(request):
+    auth.logout(request)
+    return redirect('index')
+
+@method_decorator(login_required, name='dispatch')
+class HomeOrganizerView(View):
+  # 显示赛事方主页
+  @staticmethod
+  def get(request):
+    try:
+      organizer = request.user.organizer_profile
+    except:
+      # 403 permission denied
+      return redirect('index')
+
+    # 该主办方创建的所有比赛，按比赛开始注册时间逆序排列
+    tournaments = Tournament.objects.filter(organizer=organizer).order_by('-register_begin_time')
+
+    data = dict()
+
+    data['user'] = request.user
+
+    # 参加该主办方主办的所有比赛的所有队伍数
+    data['total_team_num'] = 0
+    for tournament in tournaments:
+      data['total_team_num'] += len(tournament.team_set.all())
+
+    # 已保存的比赛
+    data['tournaments_saved'] = Tournament.objects.filter(status=Tournament.STATUS_SAVED)
+
+    # 已结束的比赛
+    data['tournaments_finished'] = Tournament.objects.filter(status=Tournament.STATUS_PUBLISHED,
+                                                             overall_end_time__lte=timezone.now())
+
+    # 已结束的比赛数目
+    data['tournament_finished_num'] = len(data['tournaments_finished'])
+
+    # 即将开始的比赛
+    data['tournaments_coming'] = Tournament.objects.filter(status=Tournament.STATUS_PUBLISHED,
+                                                           register_begin_time__gte=timezone.now())
+
+    # 即将开始的比赛数目
+    data['tournament_coming_num'] = len(data['tournaments_coming'])
+
+    # 正在进行的比赛
+    data['tournaments_ongoing'] = Tournament.objects.filter(status=Tournament.STATUS_PUBLISHED,
+                                                            register_begin_time__lte=timezone.now())
+
+    # 正在进行的比赛数目
+    data['tournament_ongoing_num'] = len(data['tournaments_ongoing'])
+
+    return render(request, 'organizer_home.html', data)
+
+
+class ProfileOrganizerView(View):
+  # 显示赛事方信息
+  @staticmethod
+  def get(request, *args):
+    user_id = args[0]
+    data = dict()
+
+    try:
+      user = User.objects.get(id=user_id)
+      organizer = user.organizer_profile
+    except:
+      return redirect('404')
+    data['organizer'] = organizer
+
+    tournaments_coming = tool.get_conditional_tournaments(organizer=organizer, type='COMING')
+    tournaments_ongoing = tool.get_conditional_tournaments(organizer=organizer, type='ONGOING')
+    tournaments_finished = tool.get_conditional_tournaments(organizer=organizer, type='FINISHED')
+
+    data['tournament_ongoing_num'] = tournaments_ongoing.count()
+    ongoing_team_num = 0
+    for tournament in tournaments_ongoing:
+      ongoing_team_num += tournament.team_set.count()
+    data['ongoing_team_num'] = ongoing_team_num
+    data['tournament_finished_num'] = tournaments_finished.count()
+    finished_team_num = 0
+    for tournament in tournaments_finished:
+      finished_team_num += tournament.team_set.count()
+    data['total_team_num'] = ongoing_team_num + finished_team_num
+
+    # TODO 贡献度算法
+    data['contribution'] = 0.5
+
+    data['tournaments_recent'] = tournaments_ongoing | tournaments_coming
+    data['tournaments_faraway'] = tournaments_finished
+
+    return render(request, 'organizer_profile.html', data)
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileEditOrganizerView(View):
+  @staticmethod
+  def get(request):
+    fields = ['organization', 'biography', 'description', 'location', 'field', 'website']
+    data = tool.load_model_obj_data_to_dict(request.user.organizer_profile, fields)
+    form = ProfileOrganizerForm(initial=data)
+    return render(request, 'organizer_profile_edit.html', {'form': form})
+
+  @staticmethod
+  def post(request):
+    form = ProfileOrganizerForm(request.POST, request.FILES)
+    if form.is_valid():
+      # 保存除avatar之外的所有field
+      fields = ['organization', 'biography', 'description', 'location', 'field', 'website']
+      tool.save_form_data_to_model_obj(request.user.organizer_profile, form, fields)
+
+      # 保存avatar
+      avatar = Image()
+      avatar.image = form.cleaned_data['avatar']
+      avatar.type = 'P'
+      avatar.owner = request.user
+      avatar.save()
+      request.user.organizer_profile.avatar = avatar
+      request.user.organizer_profile.save()
+
+      return redirect('profile-organizer', request.user.id)
+    else:
+      return render(request, 'contestant_profile_edit.html', {'form': form})
